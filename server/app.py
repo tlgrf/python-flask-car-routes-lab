c7e0599f2a6c69b2f1b8fd75c59871020b06fdf1
Ridge <ridge.telegrafi@icloud.com>
2025-07-19 19:06:59 -0400

    added Flask routes for / and /<model>, updated README

--- server/app.py
+++ server/app.py
@@ -1,1 +1,19 @@
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+from flask import Flask
+
+app = Flask(__name__)
+
+#Setting up ‘/’ route for the base URL
+@app.route("/")
+def index():
+    return "Welcome to Flatiron Cars"
+
+#Setting up '/model' route
+@app.route("/<string:model>")
+def carmodel(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
+    
